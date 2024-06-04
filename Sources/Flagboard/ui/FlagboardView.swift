d20690f9daa3305e5252cde7ae79e8bcfd49d5ee
Oscar Sierra <oscar.sierraa@externos.bci.cl>
2024-06-04 08:33:05 -0500

    Close Button Add to the main view

--- Sources/Flagboard/ui/FlagboardView.swift
+++ Sources/Flagboard/ui/FlagboardView.swift
@@ -7,54 +7,73 @@
 
 import UIKit
 
-public class FlagboardViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+import UIKit
 
-    private var tableView: UITableView!
+public class FlagboardViewController: UIViewController {
+    
+    // MARK: - Properties
+    
     private var featureFlags: [FeatureFlagS] = []
 
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView = UITableView(frame: view.bounds, style: .plain)
+    // MARK: - UI Elements
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        view.addSubview(tableView)
+        return tableView
+    }()
+    
+    private lazy var backButton: UIButton = {
+        let button = UIButton(type: .close)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle Methods
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupConstraints()
     }
-
+    
     public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         featureFlags = FlagboardInternal.getFlags()
+        tableView.reloadData()
     }
-    // MARK: - UITableViewDataSource
-
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return featureFlags.count
+    
+    // MARK: - Setup Methods
+    
+    private func setupView() {
+        view.addSubview(tableView)
+        view.addSubview(backButton)
     }
-
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let featureFlag = featureFlags[indexPath.row]
-
-        let switchView = UISwitch()
-        switchView.tag = indexPath.row
-        switchView.addTarget(self, action: #selector(switchChanged(_:)), for: .valueChanged)
-        cell.accessoryView = switchView
-        cell.textLabel?.numberOfLines = 0
-
-        switch featureFlag.featureFlag {
-        case .booleanFlag(let param):
-            cell.textLabel?.text = param.key.value
-            switchView.isOn = param.value
-        case .intFlag(let param):
-            cell.textLabel?.text = param.key.value
-            switchView.isOn = param.value == 1 ? true : false
-        default:
-            return UITableViewCell(frame: .zero)
-        }
-
-        return cell
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
+            backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16.0),
+            backButton.heightAnchor.constraint(equalToConstant: 24.0),
+            backButton.widthAnchor.constraint(equalToConstant: 24.0),
+            
+            tableView.topAnchor.constraint(equalTo: backButton.bottomAnchor, constant: 16.0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
-
+    
+    // MARK: - Actions
+    
+    @objc private func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+    
     @objc private func switchChanged(_ sender: UISwitch) {
         let featureFlag = featureFlags[sender.tag]
         switch featureFlag.featureFlag {
@@ -67,14 +86,51 @@ public class FlagboardViewController: UIViewController, UITableViewDataSource, U
         }
     }
     
+    // MARK: - Helper Methods
+    
     private func sendNotification(key: String, isOn: Bool) {
         FlagboardInternal.save(key: key, value: isOn)
     }
+}
 
-    // MARK: - UITableViewDelegate
+// MARK: - UITableViewDataSource
 
-    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 44
+extension FlagboardViewController: UITableViewDataSource {
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return featureFlags.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let featureFlag = featureFlags[indexPath.row]
+        
+        let switchView = UISwitch()
+        switchView.tag = indexPath.row
+        switchView.addTarget(self, action: #selector(switchChanged(_:)), for: .valueChanged)
+        cell.accessoryView = switchView
+        cell.textLabel?.numberOfLines = 0
+        
+        switch featureFlag.featureFlag {
+        case .booleanFlag(let param):
+            cell.textLabel?.text = param.key.value
+            switchView.isOn = param.value
+        case .intFlag(let param):
+            cell.textLabel?.text = param.key.value
+            switchView.isOn = param.value == 1
+        default:
+            break
+        }
+        
+        return cell
     }
 }
 
+// MARK: - UITableViewDelegate
+
+extension FlagboardViewController: UITableViewDelegate {
+    
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44.0
+    }
+}
